7e98de8a23e29ba8f6dcccab282311e02216eebd
MursikVar <var.mark@bk.ru>
2025-03-05 11:57:54 +0300

    abc

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// import './styles/common.css'
+import './style/common.css'
 import './style/reset.css'
 
 
